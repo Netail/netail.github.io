b165c9f76eccd9e99b7aadfdfadb0fa8671e64e0
Netail <maikel.van.dort@gmail.com>
2022-12-19 00:11:52 +0100

    feat: improved projects

--- constants/projects.ts
+++ constants/projects.ts
@@ -7,28 +7,28 @@ export const PROJECTS: Project[] = [
         description: 'A Github Action to send the same message to multiple webhooks at once',
     },
     {
-        image: '/assets/img/frog.jpg',
+        image: '/assets/img/stock-finder.jpg',
         title: 'Albert Heijn Product Finder',
         description: 'A widget to search if a product is in the assortment of a certain store',
     },
     {
-        image: '/assets/img/frog.jpg',
+        image: '/assets/img/noodplan.jpg',
         title: 'Albert Heijn Fallback Page',
-        description: 'A fallback page for when the website is down',
+        description: 'A fallback page for when the website is down, featuring an FAQ, cached recipes, folders and store information',
     },
     {
-        image: '/assets/img/frog.jpg',
-        title: 'Weight Tracker Smart Contract',
-        description: 'A blockchain dApp interacting with an Ethereum smart contract',
-    },
-    {
-        image: '/assets/img/frog.jpg',
+        image: '/assets/img/loka2loka.jpg',
         title: 'Loka 2 Loka',
         description: 'An e-commerce platform on which farmers can sell their goods directly to customers',
     },
     {
-        image: '/assets/img/frog.jpg',
+        image: '/assets/img/weight-tracker.jpg',
+        title: 'Weight Tracker dApp + Smart Contract',
+        description: 'A blockchain dApp interacting with an Ethereum smart contract',
+    },
+    {
+        image: '/assets/img/bollie.jpg',
         title: 'Bollie',
-        description: 'A Bol.com third-party orders/inventory mobile dashboard',
+        description: 'A third-party Bol.com orders/inventory management Android application',
     }
 ];

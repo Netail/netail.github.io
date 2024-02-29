589d9243a07eeb5b784225c8c1746881f80544c7
Netail <maikel.van.dort@gmail.com>
2024-02-29 22:44:47 +0100

    fix: google search console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,19 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
-    return (
-        <>
-            <Head>
-                <title>Maikel</title>
-                <meta
-                    name='description'
-                    content='Portfolio of Maikel'
-                />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Maikel</title>
+        <meta name='description' content='Portfolio of Maikel' />
+        <meta
+          name='google-site-verification'
+          content='x0xvdH595p-ha7xSvIZ0rYH8Eti50S5c2lAlHSdFE90'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
